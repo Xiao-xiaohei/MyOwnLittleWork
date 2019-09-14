c9f9275c2b1cda29f1f6edb82bc3199ce7b16460
Xiao-xiaohei <798745160@qq.com>
2019-09-14 13:13:48 +0800

    test loss

---
+++ foo.py
@@ -0,0 +1,67 @@
+from utils import util
+from models import RSHNet
+
+import torch as t
+import torch.nn as nn
+from itertools import permutations
+
+def loss(output, label):
+	'''
+		output:
+			M [C, B, T, num_bins]
+			res_M [B, T, num_bins]
+			and
+			z [B, C]
+		label:
+			M [C, B, T, num_bins]
+			z [B, C]
+
+		compute loss using PIT for M ~ M
+		L_{Mask}: MSE?
+		L_{flag}: CrossEntropy? MSE?
+		L_{res_Mask}: Square？ or CrossEntropy that minus is heavily gg!
+	'''
+	output_flags = output[2]	# [B, C]
+	L_flag = nn.BCELoss()(output_flags, label[1])
+
+	L_resMask = t.norm(output[1], 2)
+	M = output[0]
+	C = M.shape[0]
+	# pit_mat with shape [C!, B]
+	pit_mat = t.stack([mse_loss(M, label[0], p) for p in permutations(range(C))])
+	L_mask, min_per = t.min(pit_mat, dim=0)
+	return L_mask + L_flag + L_resMask
+
+def mse_loss(obtain_m, ref_m, permutation):
+	'''
+		obtain_m: the elimated masks [C, B, T, num_bins]
+		ref_m: the reference masks [C, B, T, num_bins]
+		permutation: one permutation of C!
+	'''
+	# get a loss with shape [B, ]
+	return sum([mse(obtain_m[s], ref_m[t]) for s, t in enumerate(permutation)])
+
+def mse(ob_m, ref_m):
+	'''
+		input:
+			ob_m: [B, T, num_bins]
+			ref_m: [B, T, num_bins]
+		out:
+			loss: [B, ]
+	'''
+	return t.norm(ob_m - ref_m, p='fro', dim=[-2, -1])
+
+if __name__ == '__main__':
+	net = RSHNet()
+	x = t.rand(2, 101, 129)
+	print("{} #param: {:.2f}".format(net.name, util.ComputParameters(net)))
+	m, resm, z = net(x, 3)
+	print(m.shape, resm.shape, z.shape)
+
+	C = 3
+	ref_m = t.rand([3, 2, 101, 129])
+	ref_z = t.rand([2, 3])
+	label = [ref_m, ref_z]
+	output = [m, resm, z]
+	Loss = loss(output, label)
+	print(Loss)
