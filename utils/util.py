06006c0f5f485de7f802d123c85463693c9e9abf
Xiao-xiaohei <798745160@qq.com>
2019-09-17 21:12:26 +0800

    Not Test for CreateMixWav.

--- utils/util.py
+++ utils/util.py
@@ -41,7 +41,8 @@ def CreateMixWave(path, save_path, num_speakers, snr_range, nums, spl=8000, reve
 		return signal / s_p if s_p > 0 else signal
 
 	for num_spks in num_speakers:
-		for ii, data_type in enumerate(data_types):
+		file_info = []
+		for ii, data_type in data_types:
 			wav_pairs = []
 			tmp_num = 0
 			type_num = nums[ii]
@@ -49,38 +50,69 @@ def CreateMixWave(path, save_path, num_speakers, snr_range, nums, spl=8000, reve
 				wav_pair = random.sample(wavs[ii], num_spks)
 				wav_pair = sorted(wav_pair)
 				if wav_pair not in wav_pairs:
-					min_length = np.Inf
-					snrs = []
-					sigs = []
-					save_name = ""
-
-					for wav in wav_pair:
-						tmp_snr = GetSNR(snr_range)
-						snrs.append(tmp_snr)
-						wav_name = os.path.splitext(os.path.split(wav)[1])[0]
-						save_name += (wav_name + "_" + str(tmp_snr) + "_")
-						sample, signal = wavfile.reaf(wav)
-						assert sample == spl
-
-						signal = NormalizeSignal(signal)
-						if len(signal) < min_length:
-							min_length = len(signal)
-
-						sigs.append(signal)
-					
-					snrs[0] = 0.0
-					save_name = save_name[:-1] + ".wav"
-
-					merge_wavs = np.zeros([num_spks + 1, min_length])
-					for i in range(num_spks):
-						random_b = random.randint(0, len(sigs[i] - min_length))
-						merge_wavs[i, :] = 10 ** (snrs[i] / 20) * sigs[i][random_b:random_b + min_length]
-					merge_wavs[num_spks, :] = np.sum(merge_wavs[:-1], axis=0)
-
-					################################
-					#     go for some fooooood     #
-					#	       2019.09.14          #
-					################################
-
 					tmp_num += 1
 					wav_pairs.append(wav_pair)
+
+			for wav_pair in tqdm(wav_pairs):
+				min_length = np.Inf
+				snrs = []
+				sigs = []
+				save_name = ""
+				txt_info = ""
+
+				for wav in wav_pair:
+					tmp_snr = GetSNR(snr_range)
+					snrs.append(tmp_snr)
+					wav_name = os.path.splitext(os.path.split(wav)[1])[0]
+					save_name += (wav_name + "_" + str(tmp_snr) + "_")
+					txt_info += (wav + " " + str(tmp_snr) + " ")
+					sample, signal = wavfile.reaf(wav)
+					assert sample == spl
+
+					signal = NormalizeSignal(signal)
+					if len(signal) < min_length:
+						min_length = len(signal)
+
+					sigs.append(signal)
+
+				snrs[0] = 0.0
+				save_name = save_name[:-1] + ".wav"
+				file_info.append(txt_info + save_name)
+
+				merge_wavs = np.zeros([num_spks + 1, min_length])
+				for i in range(num_spks):
+					random_b = random.randint(0, len(sigs[i] - min_length))
+					merge_wavs[i, :] = 10 ** (snrs[i] / 20) * sigs[i][random_b:random_b + min_length]
+				merge_wavs[num_spks, :] = np.sum(merge_wavs[:-1], axis=0)
+
+				################################
+				#     go for some fooooood     #
+				#          2019.09.14          #
+				################################
+
+				max_amp = np.max(merge_wavs)
+				merge_wavs /= max_amp
+				merge_wavs *= 0.9
+				merge_wavs = merge_wavs * (2 ** 15 - 1)
+
+				save_dir = ""
+				for i in range(num_spks):
+					save_dir = save_path + str(num_spks) + "speakers_0dB/wav8k/min/" + data_type + "/s" + str(i + 1) + "/"	# not sure...
+					try:
+						os.mkdir(save_dir)
+					except BaseException:
+						pass
+					wavfile.write(save_dir + save_name, spl, merge_wavs[i].astype(np.int16))
+
+				mix_dir = save_dir[:-3] + "mix/"
+				try:
+					os.mkdir(mix_dir)
+				except BaseException:
+					pass
+				wavfile.write(mix_dir + wav_name, spl, merge_wavs[num_spks].astype(np.int16))
+
+		
+		with open(path + str(num_spks) + "speakers_" + "0dB/" + str(num_spks) + "speakers_" + "8k_0dB.txt", "w", encoding="utf-8") as f:
+			for row in file_info:
+				f.write(row)
+				f.write("\n")
